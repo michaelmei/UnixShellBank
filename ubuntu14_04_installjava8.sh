b2b58d760ed1360e385644a9a36044619727fe8a
michaelmei <michael811002@gmail.com>
2015-09-22 17:41:48 +0800

    added install java and mongo for ubuntu 14.04

---
+++ ubuntu14_04_installjava8.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+java -version
+
+sudo apt-get install oracle-java8-set-default
---
+++ ubuntu14_04_quickInstallMongoDB.sh
@@ -0,0 +1,16 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo service mongod start
+
