804272426d64b1a88f804bde1bb9e49c1d15c70f
michaelmei <michael811002@gmail.com>
2015-09-22 18:01:24 +0800

    added ubuntu14_04_quickInstallRedis.sh

---
+++ ubuntu14_04_quickInstallRedis.sh
@@ -0,0 +1,16 @@
+# https://scaleyourcode.com/blog/article/3
+
+wget http://download.redis.io/releases/redis-2.8.19.tar.gz
+tar xzf redis-2.8.19.tar.gz
+sudo mv redis-2.8.19 /etc/redis
+cd /etc/redis
+sudo apt-get install make
+sudo apt-get install gcc
+sudo aptitude install build-essential
+sudo make
+
+sudo apt-get install tcl8.5
+sudo make install
+
+cd utils
+sudo ./install_server.sh
