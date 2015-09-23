4235e6fef253956f8709609732227e20f88a0a61
Michael Mei <michael811002@gmail.com>
2015-09-23 14:26:23 +0800

    Update ubuntu14_04_installjava8.sh

--- ubuntu14_04_installjava8.sh
+++ ubuntu14_04_installjava8.sh
@@ -1,7 +1,11 @@
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install oracle-java8-installer
 
-java -version
+#java -version
 
-sudo apt-get install oracle-java8-set-default
+#sudo apt-get install oracle-java8-set-default
+
+#auto lisence agreement
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
