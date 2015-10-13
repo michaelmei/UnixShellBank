a80e2e7adca8ef232dd4568aca582ba5a6809499
Michael Mei <michael811002@gmail.com>
2015-10-13 18:40:22 +0800

    Update ubuntu14_04_installjava8.sh

--- ubuntu14_04_installjava8.sh
+++ ubuntu14_04_installjava8.sh
@@ -1,3 +1,9 @@
+
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git
+
 #auto lisence agreement
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
