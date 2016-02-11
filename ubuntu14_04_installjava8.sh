279ca3c785cb91e67c09179064758a391b1b13c7
Michael Mei <michael811002@gmail.com>
2016-02-11 20:18:22 +0800

    Update ubuntu14_04_installjava8.sh
    added disable ipv6

--- ubuntu14_04_installjava8.sh
+++ ubuntu14_04_installjava8.sh
@@ -1,5 +1,14 @@
 
 sudo apt-get update
+################The security server might be stuck
+##############try following to disable the ipv6 network and then comment it back later...
+###############The IPv6 setting for the network were ignored, but disabling IPv6 via /etc/sysctl.conf worked:
+#Add the following lines at the bottom of /etc/sysctl.conf:
+########################################################################################################################
+#net.ipv6.conf.all.disable_ipv6 = 1
+#net.ipv6.conf.default.disable_ipv6 = 1
+#net.ipv6.conf.lo.disable_ipv6 = 1
+#Run sudo sysctl -p to reload the /etc/sysctl.conf settings.
 
 sudo apt-get install software-properties-common python-software-properties
 sudo add-apt-repository ppa:git-core/ppa
