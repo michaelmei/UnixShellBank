c74ff6a2557059f56dee5295722fd71b765596e8
Michael Mei <michael811002@gmail.com>
2016-03-23 11:34:41 +0800

    Update ubuntu14_04_quickInstallMongoDB.sh

--- ubuntu14_04_quickInstallMongoDB.sh
+++ ubuntu14_04_quickInstallMongoDB.sh
@@ -1,5 +1,9 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
+#Ubuntu 12.04 LTS
+#echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+#Ubuntu 14.04 LTS 
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
 sudo apt-get update
