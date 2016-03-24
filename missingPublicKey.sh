2f755a2a7d8e24e34785034c426d7e167ec26640
Michael Mei <michael811002@gmail.com>
2016-03-24 21:29:47 +0800

    Create missingPublicKey.sh

---
+++ missingPublicKey.sh
@@ -0,0 +1,7 @@
+#Run the following in your terminal,
+
+ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3C962022012520A0 
+ sudo apt-get update
+#References: How do I fix the GPG error "NO_PUBKEY"?
+
+#You need to replace the key (3C962022...) with the one that is displayed in the error message in the terminal.
