12cbf1610de8d972bc9d5ae52084efc82a2fec09
Michael Mei <michael811002@gmail.com>
2017-11-02 14:30:05 +1100

    Update sgrep.sh

--- sgrep.sh
+++ sgrep.sh
@@ -1,18 +1,46 @@
-echo "sgrep <FileName> <SearchText> <NumberOfLines>"
+echo "sgrep <FileName> <NumberOfLines> <LIMIT> <SEARCH_TEXT>"
 
-FILE=$1
-SEARCH_TEXT=$2
-CAT_LN=$3
+ARGUMENT_NUMBER=$#
+ARGUMENT_ITER=0
+for i in $*; do
+   if [ $ARGUMENT_ITER -eq 0 ]
+   then
+       FILE=$i
+   elif [ $ARGUMENT_ITER -eq 1 ]
+   then
+       CAT_LN=$i
+   elif [ $ARGUMENT_ITER -eq 2 ]
+   then
+       LIMIT=$i
+   else
+       SEARCH_TEXT="$SEARCH_TEXT $i"
+   fi
 
-LINE_NUMBERS=($(grep --line-number $SEARCH_TEXT $FILE | awk -F":" '{ print $1 }'))
+   ARGUMENT_ITER=`expr $ARGUMENT_ITER + 1`
+done
 
-for i in "${LINE_NUMBERS[@]}"
+LINE_NUMBERS=($(grep -F --line-number "$SEARCH_TEXT" $FILE | awk -F":" '{ print $1 }'))
+
+#echo LINE_NUMBERS: $LINE_NUMBERS
+
+# get length of an array
+tLen=${#LINE_NUMBERS[@]}
+loopNo=0
+
+# use for loop read all nameservers
+for (( i=0; i<$tLen; i++ ));
 do
-   :
   echo "************* $i ***************"
-        HEAD_LENGTH=`expr $i + $CAT_LN`
+	TMP_LN=${LINE_NUMBERS[$i]}
+	HEAD_LENGTH=`expr $TMP_LN + $CAT_LN`
         TAIL_LENGTH=`expr $CAT_LN + 1`
-        echo "head -n $HEAD_LENGTH $FILE | tail -n $TAIL_LENGTH"
+
+        #echo "head -n $HEAD_LENGTH $FILE | tail -n $TAIL_LENGTH"
         head -n $HEAD_LENGTH $FILE | tail -n $TAIL_LENGTH
-  echo "********************************"
+
+	loopNo=`expr $loopNo + 1`
+	if [ $loopNo -eq $LIMIT ]
+   	then
+      		break
+   	fi
 done
