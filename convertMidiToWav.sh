7c1f7680cc4243acb6d86d4581abb9669906c1b4
Michael Mei <michael811002@gmail.com>
2018-06-05 14:55:59 +1000

    Create convertMidiToWav.sh

---
+++ convertMidiToWav.sh
@@ -0,0 +1,10 @@
+#install fluidsynth
+sudo apt-get update
+sudo apt-get install fluidsynth
+
+#run command
+fluidsynth -ni sound_font.sf2 input.mid -F output.wav -r 44100
+
+#batch access via python
+https://pypi.org/project/midi2audio/
+
